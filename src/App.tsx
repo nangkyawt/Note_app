67e03316179115c0b3b80d89cba7b36c67f474c6
nangkyawt <nangkyawthsuhlaing@gmail.com>
2026-03-07 21:02:49 +0630

    my_note

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+    return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-red-500">
+        MyNote
+      </h1>
+    </div>
+  );
+}
+
+export default App
